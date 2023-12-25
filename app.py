566cf46585ff06bf71504356a2962b0744e0b70f
Dipokal HHJ <hhj@devent.kr>
2023-12-25 14:38:32 +0900

    feat: crop multiple images

--- app.py
+++ app.py
@@ -7,23 +7,25 @@ from PyQt5.QtGui import QPixmap
 from PIL import Image
 
 
-
+DEBUG_MODE = True
 MAX_CAPTURE = 3
-PRESET_PAGE = 1
+PRESET_PAGE = 2
+FILE_EXT = 'jpg'
 
 class Window(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.initUI()
+        self.init()
 
-    def initUI(self):
+    def init(self):
         self.setWindowTitle('Grading System v1')
         self.move(300, 300)
         self.resize(1000, 600)
 
         self.points = []
         self.nowPointCursor = 0
+        self.nowPageNumber = 1
 
         self.scroll = QScrollArea(self) 
         self.scroll.setGeometry(0,0,500,600)   
@@ -31,7 +33,12 @@ class Window(QWidget):
         self.label = QLabel(self)
         self.pixmap = QPixmap('./files/30725-01.jpg')
         self.label.setPixmap(self.pixmap)
-        self.label.mousePressEvent = self.getPos
+        self.label.mousePressEvent = self.getMousePosition
+
+        if (PRESET_PAGE > 1):
+            nextPageButton = QPushButton(text="Next Page", parent=self)
+            nextPageButton.move(880, 540)
+            nextPageButton.clicked.connect(self.setNextPage)
 
         for i in range(MAX_CAPTURE):
             self.addPointsInput(0, 0)
@@ -39,39 +46,64 @@ class Window(QWidget):
 
 
         self.scroll.setWidget(self.label) 
-
         self.show()
 
-    def getPos(self , event):
+
+    def getMousePosition(self , event):
         x = event.pos().x()
         y = event.pos().y() 
-        print(x, y)
+        filename = self.getNowFilename()
+
+        if (DEBUG_MODE):
+            print(x, y)
 
         self.points[self.nowPointCursor][1].setText(str(x))
         self.points[self.nowPointCursor][3].setText(str(y))
+        self.points[self.nowPointCursor][4] = filename
 
         self.nowPointCursor += 1
 
         if (self.nowPointCursor >= MAX_CAPTURE * 2):
-            self.getCropImage('./files/30725-01.jpg')
+            self.getCropImage()
 
 
-    def getCropImage(self, imageUrl):
+    def getCropImage(self):
         for i in range(0, MAX_CAPTURE * 2, 2):
-            img = Image.open(imageUrl)
+            filename = self.points[i][4]
+            print(filename)
+
+            img = Image.open("./files/" + filename)
             imgMinX = int(self.points[i][1].text())
             imgMinY = int(self.points[i][3].text())
             imgMaxX = int(self.points[i+1][1].text())
             imgMaxY = int(self.points[i+1][3].text())
 
             imgCropped = img.crop((imgMinX, imgMinY, imgMaxX, imgMaxY))
-            imgCropped.show()
+            if (DEBUG_MODE):
+                imgCropped.show()
 
+    def setNextPage(self):
+        self.nowPageNumber += 1
+        self.changePreviewImage()
+        # print(self.nowPageNumber)
 
+    def getNowFilename(self):
+        filename = "preset-{:02d}.{ext}".format(self.nowPageNumber, ext=FILE_EXT)
+        return filename
 
+    def changePreviewImage(self):
+        filename = self.getNowFilename()
+        fileUrl = "./files/" + filename
+        self.pixmap = QPixmap(fileUrl)
+        self.label.setPixmap(self.pixmap)
+        self.scroll.setWidget(self.label) 
+
+        
 
     def addPointsInput(self, x, y):
         lineNumber = len(self.points)
+        filename = self.getNowFilename()
+
 
         labelX = QLabel(self)
         labelX.move(540, 20 + (lineNumber * 20))
@@ -85,7 +117,7 @@ class Window(QWidget):
         lineY = QLineEdit(str(y), self)
         lineY.move(800, 20 + (lineNumber * 20))
 
-        self.points.append([labelX, lineX, labelY, lineY])
+        self.points.append([labelX, lineX, labelY, lineY, filename])
 
 if __name__ == '__main__':
    app = QApplication(sys.argv)
